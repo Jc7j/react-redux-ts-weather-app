29fe314c60071c129a0cdd723a12daa74aec636a
Jc7j <chiangjason19@gmail.com>
2020-02-25 22:28:11 -0800

    Changed README.md. Added some flex styling to Today page and created TodaysForecast component

---
+++ src/components/TodaysForecast.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import 'styles/TodaysForecast.css';
+
+const TodaysForecast = () => {
+  return <div>Hello world</div>;
+};
+
+export default TodaysForecast;
--- src/containers/Today.tsx
+++ src/containers/Today.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import '../styles/Today.css';
+
+import TodaysForecast from 'components/TodaysForecast';
+
+import 'styles/Today.css';
 
 const Today = () => {
   return (
     <div className="todayContainer">
-      <div className="todayInnerContainer">Welcome</div>
+      <div className="todayInnerContainer">
+        <div className="todayHeader">
+          <strong>Welcome</strong>
+        </div>
+        <TodaysForecast />
+        <div className="todayFooter">Bye</div>
+      </div>
     </div>
   );
 };

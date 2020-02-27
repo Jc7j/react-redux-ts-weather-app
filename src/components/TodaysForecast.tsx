f9a6212c98550bc09a316de504eeeebfc1eebc1b
Jc7j <chiangjason67@gmail.com>
2020-02-26 22:04:34 -0800

    Added date and city name, state name in TodaysForecast

--- src/components/TodaysForecast.tsx
+++ src/components/TodaysForecast.tsx
@@ -1,11 +1,25 @@
+import { format } from 'date-fns';
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { forecastCitySelector } from 'store/forecast/selectors';
+import { selectedStateSelector } from 'store/selection/selectors';
 
 import 'styles/TodaysForecast.css';
 
 const TodaysForecast = () => {
+  const city = useSelector(forecastCitySelector);
+  const { stateName } = useSelector(selectedStateSelector);
+
   return (
     <div className="todaysForecastContainer">
-      <h2>Todadfsfydssddss</h2>
+      <h1>Today</h1>
+      <p className="subheadingText">{format(new Date(), 'eee e, LLL')}</p>
+      {city.name && stateName && (
+        <p className="subheadingText">
+          {city.name}, {stateName}
+        </p>
+      )}
     </div>
   );
 };

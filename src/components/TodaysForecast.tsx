0b9c8a8a993eb31f0aa51e8662af4d22306f2382
Jc7j <chiangjason67@gmail.com>
2020-02-26 21:15:53 -0800

    Anothere test

--- src/components/TodaysForecast.tsx
+++ src/components/TodaysForecast.tsx
@@ -5,7 +5,7 @@ import 'styles/TodaysForecast.css';
 const TodaysForecast = () => {
   return (
     <div className="todaysForecastContainer">
-      <h2>Todaffydsdss</h2>
+      <h2>Todadffydsdss</h2>
     </div>
   );
 };

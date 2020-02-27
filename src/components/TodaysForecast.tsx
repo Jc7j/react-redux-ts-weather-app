41554ec9945c1287d105ea419cd7ac87c2280015
Jc7j <chiangjason67@gmail.com>
2020-02-26 20:57:01 -0800

    Another test test...

--- src/components/TodaysForecast.tsx
+++ src/components/TodaysForecast.tsx
@@ -5,7 +5,7 @@ import 'styles/TodaysForecast.css';
 const TodaysForecast = () => {
   return (
     <div className="todaysForecastContainer">
-      <h2>Todayds</h2>
+      <h2>Todaydsds</h2>
     </div>
   );
 };

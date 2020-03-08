2f37d56f4db16f159cf5b7948894627c4a55bc32
Jc7j <chiangjason67@gmail.com>
2020-03-07 18:19:58 -0800

    Put weatherApiKey into the request file instead of .env

--- src/api/request.ts
+++ src/api/request.ts
@@ -8,12 +8,14 @@ export const useWeatherApi = (
 ): WeatherApiModel | any => {
   const [data, setData] = useState(null);
 
+  const weatherAPIKey = '408f95afb6ad51e81124d1dfd96f9fce';
+
   useEffect(() => {
     const fetchData = async (): Promise<any | undefined> => {
       try {
         const result = await axios({
           method: 'get',
-          url: `https://api.openweathermap.org/data/2.5/forecast?q=${cityName},${countryCode}&APPID=${process.env.REACT_APP_WEATHER_APP_KEY}`
+          url: `https://api.openweathermap.org/data/2.5/forecast?q=${cityName},${countryCode}&APPID=${weatherAPIKey}`
         });
 
         setData(result.data);
